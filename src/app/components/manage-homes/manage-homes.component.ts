31e106e67a181d1c6d44efc71d36b29c1a3bf00e
matejserbec <matej.serbec@gmail.com>
2016-07-25 15:32:48 -0700

    added all components for all pages and deleted code we wont be using

---
+++ src/app/components/manage-homes/manage-homes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'as-manage-homes',
+    templateUrl: 'app/components/manage-homes/manage-homes.component.html',
+})
+
+export class ManageHomesComponent {
+
+}
