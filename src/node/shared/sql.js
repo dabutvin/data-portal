b84a4a1a698afbce14d27f3a60e09896b4327e8e
Brandyn Bayes <brandyn.bayes@gmail.com>
2016-07-25 22:08:02 -0700

    Added SQL Azure support

--- src/node/shared/sql.js
+++ src/node/shared/sql.js
@@ -3,8 +3,17 @@ var env = require('./env');
 var keyVault = require('./keyvault');
 
 keyVault.getSecret(env.sqlSecretName).then(function (result) {
-    console.log(result);
-    var config = result;
+    var config = result.value;
+
+    sql.connect(config).then(function () {
+        new sql.Request().query("SELECT * FROM SOME_TABLE'").then(function (recordSet) {
+            console.log(recordSet);
+        }).catch(function (err) {
+            console.error(err);
+        });
+    }).catch(function (err) {
+        console.error(err);
+    });
 });
 
 module.exports = {
